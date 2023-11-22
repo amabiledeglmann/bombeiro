29f7c8f2efaf1278021813c512259d467c0ff37b
amabiledeglmann <124676680+amabiledeglmann@users.noreply.github.com>
2023-11-22 09:01:41 -0300

    Add files via upload

---
+++ observacoes.php
@@ -0,0 +1,14 @@
+<?php
+include("conecta.php");
+$caixaobserva= $_POST["caixaobserva"];
+
+
+
+
+$comando = $pdo->prepare("INSERT INTO observacoes VALUES ('$caixaobserva')");
+$resultado = $comando->execute();
+header("Location:cinematica.html");
+
+
+
+?>
